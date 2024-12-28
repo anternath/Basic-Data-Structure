b797c173107b4fdcae9ff6d31b860a7ab2fa4c55
Anter Nath <anternath73@gmail.com>
2024-12-28 22:39:16 +0600

    time complexity

---
+++ o_1_complexity.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=1; //for constant variable time complexity is O(1)
+    int b=20;//O(1)
+    int d=243;//O(1)
+    cout<<a<<" "<<b<<" "<<d<<" "<<endl;
+    //total complexity O(1);
+}
---
+++ o_n_complexity.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=0; i<n; i++){// time complexity = o(n)
+        cout<<"O(n)"<<endl;
+    }
+    for(int i=0; i<m; i++){//time complexity = o(m)
+        cout<<"O(m)"<<endl;
+    }
+    //Total time complexity = O(n+m)
+}
