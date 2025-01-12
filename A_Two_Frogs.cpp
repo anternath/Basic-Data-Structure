ecbd969a6429b6cf5a3f2e843bbbd5037ed60b95
Anter Nath <anternath73@gmail.com>
2025-01-12 22:10:39 +0600

    Linked list

---
+++ A_Two_Frogs.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int n,a,b;
+    while(t--){
+        
+        cin>>n>>a>>b;
+        if(n>2){
+            bool flag=false;
+            for(int i=1; i<=n; i++){
+                if(a+1<=n&& a-1>=n){
+                    if(a+1==b&&a-1==b){
+                    flag= true;
+                }
+                }
+                if(flag==true){
+                cout<<"YES"<<endl;
+            }
+            }
+            
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
