dcf07b02a6266b469b74697c50567873971dad33
Anter Nath <anternath73@gmail.com>
2025-01-22 02:08:43 +0600

    reverse print & sorting linked list

---
+++ sorting_linked_list.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+void insert_at_tail(Node* &head,Node* &tail,int val){//time complexity O(1);
+    Node* newnode= new Node(val);
+    if(head==NULL){
+        head=newnode;
+        tail=head;
+    }
+    else{
+        tail->next=newnode;
+        tail= newnode;
+    }
+}
+void print_linked_list(Node *head){
+    Node * tem= head;
+    while(tem!=NULL){
+        cout<<tem->val<<" ";
+        tem= tem->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Node* head= NULL;
+    Node* tail= NULL;
+    int v;
+    while(true){
+        cin>>v;
+        if(v==-1) break;
+        insert_at_tail(head,tail,v);
+    }
+    for(Node* i=head; i->next!=NULL; i=i->next){
+        for(Node* j=i->next; j!=NULL; j=j->next){
+            if(i->val>j->val){
+                swap(i->val,j->val);
+            }
+        }
+    }
+    
+    print_linked_list(head);
+    return 0;
+}
