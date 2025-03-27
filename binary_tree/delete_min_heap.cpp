504828a2785c8cb7168a4ae39ded018649ccf2d5
Anter Nath <anternath73@gmail.com>
2025-03-28 01:50:50 +0600

    heap

--- binary_tree/delete_min_heap.cpp
+++ binary_tree/delete_min_heap.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-void insert_in_heap(vector<int> &v,int val){
-    for(int i=0; i<val; i++){
-        int x;
-        cin>>x;
-        v.push_back(x);
-        int cur_indx= v.size()-1;
-        while(cur_indx!=0){
-        
-        int par_indx= (cur_indx-1)/2;
-        if(v[par_indx]<v[cur_indx]){
-            swap(v[par_indx],v[cur_indx]);
-            cur_indx=par_indx;
+void insert_node(vector<int> &v, int n){
+    int x;
+    for(int i=0; i<n; i++){
+       cin>>x;
+       v.push_back(x);
+       int cur_idx= v.size()-1;
+    
+    while(cur_idx!=0){
+        int par_idx=(cur_idx-1)/2;
+        if(v[cur_idx]<v[par_idx]){
+            swap(v[cur_idx],v[par_idx]);
+            cur_idx=par_idx;
         }
         else break;
-        
+    } 
     }
-}
+    
 }
 void delete_heap(vector<int>&v){
     swap(v[0],v[v.size()-1]);
@@ -27,25 +27,25 @@ void delete_heap(vector<int>&v){
         int right_idx= cur_idx*2+2;
         int last_idx= v.size()-1;
         if(left_idx<=last_idx && right_idx<=last_idx){
-            if(v[left_idx]>= v[right_idx]&& v[left_idx]> v[cur_idx]){
+            if(v[left_idx]<= v[right_idx]&& v[left_idx]< v[cur_idx]){
                 swap(v[cur_idx],v[left_idx]);
                 cur_idx=left_idx;
             }
-            else if(v[right_idx]>= v[left_idx]&& v[right_idx]>v[cur_idx]){
+            else if(v[right_idx]<= v[left_idx]&& v[right_idx]<v[cur_idx]){
                     swap(v[cur_idx],v[right_idx]);
                     cur_idx=right_idx;
             }
             else break;
         }
         else if(left_idx<=last_idx){
-                if(v[left_idx]>v[cur_idx]){
+                if(v[left_idx]<v[cur_idx]){
                     swap(v[left_idx],v[cur_idx]);
                     cur_idx=left_idx;
                 }
                 else break;
         }
         else if(right_idx<=last_idx){
-            if(v[right_idx]>v[cur_idx]){
+            if(v[right_idx]<v[cur_idx]){
                 swap(v[right_idx],v[cur_idx]);
                 cur_idx=left_idx;
             }
@@ -55,13 +55,13 @@ void delete_heap(vector<int>&v){
     }
 }
 int main(){
+    vector<int>v;
     int n;
     cin>>n;
-    vector<int>v;
-    insert_in_heap(v,n);
+    insert_node(v,n);
     delete_heap(v);
-    for(int val:v){
-        cout<<val<<" ";
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
     }
     return 0;
 }
